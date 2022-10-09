197997ef436010dece03dd8fa3685c6162f506ca
Gonzalo Arca <goarca@itba.edu.ar>
2022-10-09 13:02:47 -0300

    Add HUD with crosshair + Import Skeleton, Synty and Wireframe Mesh packs

---
+++ Deathwave/Assets/Scripts/Strategy/IMovable.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IMovable
+{
+    float MovementSpeed { get; }
+    float RotationSpeed { get; }
+    
+    
+}
