5185175613603b356c481c1e1397b4ca23430677
Gonzalo Arca <goarca@itba.edu.ar>
2022-11-17 21:43:59 -0300

    add round to UI

--- Assets/Scripts/Managers/RoundManager.cs
+++ Assets/Scripts/Managers/RoundManager.cs
@@ -10,28 +10,33 @@ namespace Managers
         [SerializeField] private int _maxRounds = 5;
         [SerializeField] private bool _isVictory = false;
         [SerializeField] private Sprite _image;
-     
+
         public int _currentRound;
         private int _maxEnemies = 5;
         private int _roundKills = 0;
-        
+
         private SpawnManager spawnManager;
 
         void Start()
         {
             spawnManager = gameObject.GetComponent<SpawnManager>();
-            _currentRound = 0;
+            _currentRound = 1;
+            EventsManager.Instance.EventRoundChange(_currentRound);
             EventsManager.Instance.OnEnemyDeath += OnEnemyDeath;
             spawnManager.Reset(_maxEnemies);
         }
 
-        
-        void OnEnemyDeath(){
-            _roundKills +=1;
-            if(_roundKills >= _maxEnemies){
-                _maxEnemies = (int) (_maxEnemies * 1.25);
+
+        void OnEnemyDeath()
+        {
+            _roundKills += 1;
+
+            if (_roundKills >= _maxEnemies)
+            {
+                _maxEnemies = (int)(_maxEnemies * 1.25);
                 _roundKills = 0;
-                _currentRound +=1;
+                _currentRound += 1;
+                EventsManager.Instance.EventRoundChange(_currentRound);
                 spawnManager.Reset(_maxEnemies);
             }
         }
